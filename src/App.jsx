cea219c4c89480b82e5b9f0a409e9ea3f65992ed
kkk1k <googija9818@gmail.com>
2025-01-16 18:28:42 +0900

    route 정리리

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Ref from "./Ref";
 import Home from "./Home";
+import SseComponent from "./SseComponent";
+import SocketComponent from "./SocketComponent";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/ref" element={<Ref />} />
+      <Route path="/sse" element={<SseComponent />} />
+      <Route path="/socket" element={<SocketComponent />} />
     </Routes>
   );
 }
